e93f327b8cf249db820adc3f16d41ebe0b18328c
Grui <836162880@qq.com>
2017-11-04 12:06:08 +0800

    kml files from google earth

---
+++ geometryTools/kml_to_txt.py
@@ -0,0 +1,13 @@
+# -*- encoding:utf-8 -*-
+# !/usr/bin/env python
+
+import os
+# 列出当前目录下所有的文件
+files = os.listdir(".")
+for filename in files:
+    portion = os.path.splitext(filename)
+    # 如果后缀是.kml
+    if portion[1] == ".kml":
+        # 重新组合文件名和后缀名
+        newname = portion[0] + ".txt"
+        os.rename(filename, newname)

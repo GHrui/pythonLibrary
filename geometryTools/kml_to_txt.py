96f7693b2c88c561b8ae1858de0ea9ebd88a7bbd
Grui <836162880@qq.com>
2017-11-04 13:58:29 +0800

    This program converts each polygons' kml files to shp files.

--- geometryTools/kml_to_txt.py
+++ geometryTools/kml_to_txt.py
@@ -3,11 +3,14 @@
 
 import os
 # 列出txt目录下所有的文件
-files = os.listdir(".")
-for filename in files:
-    portion = os.path.splitext(filename)
-    # 如果后缀是.kml
-    if portion[1] == ".kml":
-        # 重新组合文件名和后缀名
-        newname = portion[0] + ".txt"
-        os.rename(filename, newname)
+path_list = ["\\Demo\\txt\\point", "\\Demo\\txt\\polygon", "\\Demo\\txt\\polyline"]
+for path in path_list:
+    files = os.listdir("." + path)
+    dir_name = os.path.dirname(os.path.abspath(files[0]))
+    for filename in files:
+        portion = os.path.splitext(filename)
+        # 如果后缀是.kml
+        if portion[1] == ".kml":
+            # 重新组合文件名和后缀名
+            newname = portion[0] + ".txt"
+            os.rename(dir_name + path + "\\" + filename, dir_name + path + "\\" + newname)
